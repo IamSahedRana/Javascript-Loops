7458533aa1e54fd64bd222e5c840e92bed36bcd5
IamSahedRana <sahed.vmc@gmail.com>
2026-07-18 23:17:11 +0600

    Basic Calculation of For Loop

---
+++ exp.js
@@ -0,0 +1,13 @@
+ 
+
+/* console.log('Let Start Print');
+for( i = 10; i >= 0; i--) {
+     console.log(i);
+}
+console.log('Finish'); */
+
+const count = 3; 
+
+for (i = 0; i <= 10; i++ ){
+     console.log(count, 'x', i, '=',i*count)
+}
