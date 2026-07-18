142c12efcefc5f71bda74c6a1fa9a7797d1d0977
IamSahedRana <sahed.vmc@gmail.com>
2026-07-18 10:10:10 +0600

    For Loop Recap and Practice

---
+++ for.js
@@ -0,0 +1,13 @@
+/* 
+ For Loop Practice
+*/
+
+
+// for (let i = 10; i <30; i++) {
+//      console.log('The Value is: ', i);
+// }
+
+
+for (let x =29; x >= 9; x--) {
+     console.log('The Value is: ',x );
+}
