67e9229e99c1574d3d99dc667a3ae52b9ce55bc6
IamSahedRana <sahed.vmc@gmail.com>
2026-07-18 16:16:01 +0600

    Practice While and For Loops

---
+++ practice.js
@@ -0,0 +1,24 @@
+/* Even Number Calculation */
+
+/* let sum = 0; 
+
+for (let i = 0; i < 50; i++) {
+     sum = sum + 2;
+     console.log('The value of the number:', sum);
+} */
+
+
+/* +5 Number Calculation till 100 */
+
+
+
+/* for (count = 5; count <= 100; count = count +5) {
+     console.log("The Next Number is:", count)
+} */
+
+let sum = 5;
+
+while (sum <= 100) {
+     console.log("The Next number is: ", sum);
+     sum = sum +5;
+}
