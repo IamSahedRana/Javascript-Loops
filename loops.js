ef1b9d0938267d174b6ea3ea001a29d438fe52dd
IamSahedRana <sahed.vmc@gmail.com>
2026-07-18 09:29:38 +0600

    Learn Loops Structure and Basics

---
+++ loops.js
@@ -0,0 +1,18 @@
+/* 
+* 1. Starting Situation // Let count = 0;
+* 2. Count Value Adapt // count < 10;
+* 3. Stopping condition (untill ends or fixed number of times) // count < 10;
+* 4. Repetitive same or similar task // count ++;
+* 5. See the Output // Console.log("Hello, MAMA!");
+*/
+
+/* 
+
+for ( Initialization ; Condition ; Modification) {
+     Repeatitive Tasks
+}
+*/
+
+for (let count = 0; count < 20 ; count++) {
+     console.log("Hello, MAMA!")
+}
