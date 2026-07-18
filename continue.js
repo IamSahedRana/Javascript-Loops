4edbf84dbd18c72672b3dc628524bab7a67afa90
IamSahedRana <sahed.vmc@gmail.com>
2026-07-18 23:51:01 +0600

    Javascript Continue usecases into the loops as statement

---
+++ continue.js
@@ -0,0 +1,10 @@
+
+// countine also means skip the number
+// ODD Number Calculation 
+
+for (let i = 0; i <= 10; i++ ){
+     if (i % 2 === 0){
+          continue;
+     }
+     console.log('The Number is: ', i);
+}
